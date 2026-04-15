337109d3e8ca25bed9f44ac2596ab5e383b8af6c
koreyhildebrand <koreyhildebrand@gmail.com>
2026-04-15 16:24:44 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+"""Configuration constants for the St. Vital Mustangs Registration Portal."""
+
+VERSION = "v4.00"  # Refactored modular version
+PAGE_ICON = "🏈"
+TITLE = "St. Vital Mustangs Registration Portal"
+COOKIE_NAME = "stvital_mustangs_portal"
+COOKIE_KEY = "super_secret_key_2026_mustangs"
+COOKIE_EXPIRY_DAYS = 30
