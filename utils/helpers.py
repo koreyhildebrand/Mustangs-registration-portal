5c6d8a492e92feb7701e3888ca677a3b7d944ebe
koreyhildebrand <koreyhildebrand@gmail.com>
2026-04-15 16:46:54 -0500

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -10,20 +10,22 @@ def to_bool(val) -> bool:
 
 
 def calculate_age_group(dob_str: str, season_year: int) -> str:
-    """Calculate age group (U10/U12/etc.) for a given season year."""
+    """Calculate age group using the NEW birth-year spec you provided."""
     try:
         dob_str = str(dob_str).strip()
         if '/' in dob_str:
             dob = datetime.datetime.strptime(dob_str, "%m/%d/%Y").date()
         else:
             dob = datetime.datetime.strptime(dob_str.split()[0], "%Y-%m-%d").date()
+        
         age = season_year - dob.year
-        if 9 <= age <= 10:   return "U10"
-        elif 11 <= age <= 12: return "U12"
-        elif 13 <= age <= 14: return "U14"
-        elif 15 <= age <= 16: return "U16"
-        elif 17 <= age <= 18: return "U18"
-        elif age >= 19:      return "Major"
+        
+        if 8 <= age <= 9:   return "U10"
+        elif 10 <= age <= 11: return "U12"
+        elif 12 <= age <= 13: return "U14"
+        elif 14 <= age <= 15: return "U16"
+        elif 16 <= age <= 17: return "U18"
+        elif age >= 18:      return "Major"
         return f"Outside {season_year}"
     except:
         return "Invalid"
